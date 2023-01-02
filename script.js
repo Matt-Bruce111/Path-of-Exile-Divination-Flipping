5fac11d399828d75f40216b1fc67237a2acf70b9
Matt-Bruce111 <Matt.bruce111@gmail.com>
2023-01-02 18:55:11 +1100

    Fixed a bug with single card sets

--- script.js
+++ script.js
@@ -13,9 +13,17 @@ function getDivCards(){
 
 function getSetValue(data){
     for(var i = 0; i < 50; i++){
-        setValue = Math.round(data.lines[i].stackSize * data.lines[i].chaosValue)
-        console.log(setValue)
-        data.lines[i].setValue = setValue
+        if(data.lines[i].hasOwnProperty("stackSize")){
+            data.lines[i].chaosValue = Math.round(data.lines[i].chaosValue)
+            setValue = Math.round(data.lines[i].stackSize * data.lines[i].chaosValue)
+            console.log(setValue)
+            data.lines[i].setValue = setValue
+        }else{
+            data.lines[i].stackSize = 1
+            setValue = Math.round(data.lines[i].stackSize * data.lines[i].chaosValue)
+            console.log(setValue)
+            data.lines[i].setValue = setValue
+        }   
     }
 }
 
@@ -25,7 +33,7 @@ function buildTable(data){
 
     for (var i = 0; i < 50; i++){
         var row = `<tr>
-                        <td><img class="css-mpit4l" width="30" height="30" src="https://web.poecdn.com/gen/image/WzI1LDE0LHsiZiI6IjJESXRlbXMvRGl2aW5hdGlvbi9JbnZlbnRvcnlJY29uIiwidyI6MSwiaCI6MSwic2NhbGUiOjF9XQ/f34bf8cbb5/InventoryIcon.png" alt=""><span>  </span>${data.lines[i].name}</td>
+                        <td><img class="css-mpit4l" title="Divination Card" width="30" height="30" src="https://web.poecdn.com/gen/image/WzI1LDE0LHsiZiI6IjJESXRlbXMvRGl2aW5hdGlvbi9JbnZlbnRvcnlJY29uIiwidyI6MSwiaCI6MSwic2NhbGUiOjF9XQ/f34bf8cbb5/InventoryIcon.png" alt=""><span>  </span>${data.lines[i].name}</td>
                         <td class="data-right">${data.lines[i].stackSize}</td>
                         <td class="data-right">${data.lines[i].chaosValue}<span>  </span><img title="Chaos Orb" width="25" height="25" src="https://web.poecdn.com/image/Art/2DItems/Currency/CurrencyRerollRare.png?scale=1&w=1&h=1" alt="Chaos Orb"></td>
                         <td class="data-right">${data.lines[i].setValue}<span>  </span><img title="Chaos Orb" width="25" height="25" src="https://web.poecdn.com/image/Art/2DItems/Currency/CurrencyRerollRare.png?scale=1&w=1&h=1" alt="Chaos Orb"></td>               
