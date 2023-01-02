1c48c67cda2f5b3da8d63a5485beb509e6b9488a
Matt-Bruce111 <Matt.bruce111@gmail.com>
2023-01-02 16:10:05 +1100

    Finished functions for fetching div data and adding it to the table

---
+++ script.js
@@ -0,0 +1,37 @@
+// Get All
+function getDivCards(){
+    $.ajax({
+    url: "https://poe.ninja/api/data/itemoverview?league=Sanctum&type=DivinationCard&language=en",    
+    method: "GET",
+    success:function(response){
+        data = response
+        getSetValue(data)
+        buildTable(data)
+        console.log(data)
+    }})
+}
+
+function getSetValue(data){
+    for(var i = 0; i < 50; i++){
+        setValue = Math.round(data.lines[i].stackSize * data.lines[i].chaosValue)
+        console.log(setValue)
+        data.lines[i].setValue = setValue
+    }
+}
+
+function buildTable(data){
+    var table = document.getElementById("div-data")
+    table.innerHTML = "";
+
+    for (var i = 0; i < 50; i++){
+        var row = `<tr>
+                        <td><img class="css-mpit4l" width="30" height="30" src="https://web.poecdn.com/gen/image/WzI1LDE0LHsiZiI6IjJESXRlbXMvRGl2aW5hdGlvbi9JbnZlbnRvcnlJY29uIiwidyI6MSwiaCI6MSwic2NhbGUiOjF9XQ/f34bf8cbb5/InventoryIcon.png" alt=""><span>  </span>${data.lines[i].name}</td>
+                        <td class="data-right">${data.lines[i].stackSize}</td>
+                        <td class="data-right">${data.lines[i].chaosValue}<span>  </span><img title="Chaos Orb" width="25" height="25" src="https://web.poecdn.com/image/Art/2DItems/Currency/CurrencyRerollRare.png?scale=1&w=1&h=1" alt="Chaos Orb"></td>
+                        <td class="data-right">${data.lines[i].setValue}<span>  </span><img title="Chaos Orb" width="25" height="25" src="https://web.poecdn.com/image/Art/2DItems/Currency/CurrencyRerollRare.png?scale=1&w=1&h=1" alt="Chaos Orb"></td>               
+                    </tr>`
+        table.innerHTML += row
+    }
+}
+
+getDivCards();
